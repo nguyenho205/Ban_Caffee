c784389ebaffb13d75747d10ed9aaa55d5735bf7
nguyenho205 <nguyenhobd.2005@gmail.com>
2025-10-30 14:12:24 +0700

    fix

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -18,16 +18,7 @@ namespace LTWeb_Buoi10.Controllers
         [HttpPost]
         public ActionResult LoginSubmit(FormCollection collection)
         {
-            var email = collection["email"];
-            var password = collection["password"];
-
-            tblKhachHang kh = db.tblKhachHang.FirstOrDefault(x => x.Email == email && x.MatKhau == password);
-            if (kh != null)
-            {
-                Session["User"] = kh;
-                return RedirectToAction("Index", "Home");
-            }
-            ViewBag.Error = "Email hoặc mật khẩu không đúng!";
+            
 
             return View("Login");
         }
@@ -36,20 +27,9 @@ namespace LTWeb_Buoi10.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult RegisterSubmit(FormCollection collection, tblKhachHang kh)
+        public ActionResult RegisterSubmit(FormCollection collection)
         {
-            var name = collection["name"];
-            var email = collection["email"];
-            var password = collection["password"];
-
-
-            kh.TenKH = name;
-            kh.Email = email;
-            kh.MatKhau = password;
-
-
-            db.tblKhachHang.Add(kh);
-            db.SaveChanges();
+            
             return RedirectToAction("Login");
         }
 
